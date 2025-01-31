9e7563085628b2fb3f86589dfb5bbf1fd6833420
Andrew Anderson <andruy@gmail.com>
2025-01-31 12:51:10 -0500

    remove old canvas background

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Menu from './components/Menu'
 function App() {
     return (
         <>
-            {/* <canvas className="background-canvas"></canvas> */}
             <Menu />
         </>
     )
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -57,21 +57,21 @@ function Menu() {
     return (
         <>
             <div className="btn-group-vertical">
-                <ModalButton number={apple.id} title={apple.name} onClick={() => {}} />
-                <ModalButton number={microsoft.id} title={microsoft.name} onClick={() => {if (microsoft.ref.current) microsoft.ref.current.getDirectories()}} />
-                <ModalButton number={folder.id} title={folder.name} onClick={() => {}} />
-                <ModalButton number={hourglass.id} title={hourglass.name} onClick={() => {}} />
-                <ModalButton number={calendar.id} title={calendar.name} onClick={() => {if (calendar.ref.current) calendar.ref.current.getActions()}} />
-                <ModalButton number={clipboard.id} title={clipboard.name} onClick={() => {if (clipboard.ref.current) clipboard.ref.current.gatherTaskList()}} />
-                <ModalButton number={pollo.id} title={pollo.name} onClick={() => {}} />
+                <ModalButton number={apple.id} title={apple.name} onClick={() => { }} />
+                <ModalButton number={microsoft.id} title={microsoft.name} onClick={() => { if (microsoft.ref.current) microsoft.ref.current.getDirectories() }} />
+                <ModalButton number={folder.id} title={folder.name} onClick={() => { }} />
+                <ModalButton number={hourglass.id} title={hourglass.name} onClick={() => { }} />
+                <ModalButton number={calendar.id} title={calendar.name} onClick={() => { if (calendar.ref.current) calendar.ref.current.getActions() }} />
+                <ModalButton number={clipboard.id} title={clipboard.name} onClick={() => { if (clipboard.ref.current) clipboard.ref.current.gatherTaskList() }} />
+                <ModalButton number={pollo.id} title={pollo.name} onClick={() => { }} />
             </div>
-            <Modal number={apple.id} title={apple.name} ref={apple.ref} Content={Apple} />
-            <Modal number={microsoft.id} title={microsoft.name} ref={microsoft.ref} Content={Microsoft} />
-            <Modal number={folder.id} title={folder.name} ref={folder.ref} Content={Folder} />
-            <Modal number={hourglass.id} title={hourglass.name} ref={hourglass.ref} Content={Hourglass} />
-            <Modal number={calendar.id} title={calendar.name} ref={calendar.ref} Content={Calendar} />
-            <Modal number={clipboard.id} title={clipboard.name} ref={clipboard.ref} Content={Clipboard} />
-            <Modal number={pollo.id} title={pollo.name} ref={pollo.ref} Content={Pollo} />
+            <Modal number={apple.id} title={apple.name} Content={Apple} ref={apple.ref} />
+            <Modal number={microsoft.id} title={microsoft.name} Content={Microsoft} ref={microsoft.ref} />
+            <Modal number={folder.id} title={folder.name} Content={Folder} ref={folder.ref} />
+            <Modal number={hourglass.id} title={hourglass.name} Content={Hourglass} ref={hourglass.ref} />
+            <Modal number={calendar.id} title={calendar.name} Content={Calendar} ref={calendar.ref} />
+            <Modal number={clipboard.id} title={clipboard.name} Content={Clipboard} ref={clipboard.ref} />
+            <Modal number={pollo.id} title={pollo.name} Content={Pollo} ref={pollo.ref} />
         </>
     )
 }
